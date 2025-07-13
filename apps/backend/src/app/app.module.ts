442dbce0941388001133b6c9da13d2b6315e3422
Darlan Raimundo <darlanraimundo123@gmail.com>
2025-07-13 10:46:29 -0300

    chore: update dependencies and add TypeORM support
    - Added @nestjs/typeorm version 11.0.0 for improved database integration.
    - Included mysql2 version 3.14.2 as a MySQL driver.
    - Added typeorm version 0.3.25 for ORM functionality.

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',        // seu usuário do MySQL
+      password: '88325536',   // sua senha do MySQL
+      database: 'gc_project',  // nome do banco de dados
+      autoLoadEntities: true,
+      synchronize: true,       // use true só em desenvolvimento!
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
